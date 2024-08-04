633cb70ca718d4ac61be034eb68514787c38e3e9
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-04 20:29:44 +0900

    node js로 할당된 request의 url(branch)에 따라 새로운 respons를 할당하도록 코드를 추가

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require('http');
 
 
-//request, response는 node.js에 의해 할당됨.
-function handleRequest(request, response)
-{
-    response.statusCode = 200; //브라우저에 요청이 성공했는지 여부를 알리는 간단한 방법 200은 성공을 의미 cf) 404
-    response.end("<h1>Hello client</h1>"); //응답 준비를 끝냄. 보낼 HTML요소를 보냄
+// request와 response는 Node.js에 의해 자동으로 할당됩니다.
+function handleRequest(request, response) {
+    if (request.url === '/currenttime') { // Node.js로 인해 할당된 request의 URL에 따라 새로운 response를 할당합니다.
+        response.statusCode = 200;
+        response.end('<h1>' + new Date().toISOString() + '</h1>'); // Date 객체를 생성하여 현재 시간을 ISO 문자열로 반환합니다.
+    } else if (request.url === '/') {
+        response.statusCode = 200; // 브라우저에 요청이 성공했는지 여부를 알리는 간단한 방법입니다. 200은 성공을 의미합니다.
+        response.end("<h1>Hello client</h1>"); // 응답 준비를 끝내고 HTML 요소를 보냅니다.
+    } else {
+        response.statusCode = 404; // 요청된 URL이 정의되지 않은 경우 404 오류를 응답합니다.
+        response.end("<h1>Page not found</h1>");
+    }
 }
+
 const server = http.createServer(handleRequest);
 
 
