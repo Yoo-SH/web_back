ddc496781f0b9d7b29a3e7e427f97348fdb5da5a
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-04 20:11:33 +0900

    server에서 client에게 response를 하는 코드를 만듦.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const http = require('http');
 
-const server = http.createServer();
+
+//request, response는 node.js에 의해 할당됨.
+function handleRequest(request, response)
+{
+    response.statusCode = 200; //브라우저에 요청이 성공했는지 여부를 알리는 간단한 방법 200은 성공을 의미 cf) 404
+    response.end("<h1>Hello client</h1>"); //응답 준비를 끝냄. 보낼 HTML요소를 보냄
+}
+const server = http.createServer(handleRequest);
+
 
 server.listen(3000);
