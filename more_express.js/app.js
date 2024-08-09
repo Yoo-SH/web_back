f7fbc8be2cfe8236d6adac8e1cd4e035c48c2972
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-09 22:32:19 +0900

    form의 데이터를 분석하여 입력값을 post하고 redirect하는 response를 보냄

--- more_express.js/app.js
+++ more_express.js/app.js
@@ -1,11 +1,11 @@
+const fs = require('fs');
 const path = require('path');
 
-const express =require('express');
-
-
+const express = require('express');
 const app = express();
-app.use(express.static('public')); //모든 수신 요청에 대해 이 공용 폴더에서 찾을 수 있는 파일에 대한 요청인지 확인해야 한다고 express에게 알림.
 
+app.use(express.static('public')); //모든 수신 요청에 대해 이 공용 폴더에서 찾을 수 있는 파일에 대한 요청인지 확인해야 한다고 express에게 알림.
+app.use(express.urlencoded({extended:false}));
 
 app.get('/', function(req,res){
     const htmlFilePath = path.join(__dirname,'views', 'index.html');
@@ -17,9 +17,31 @@ app.get('/restaurants', function(req,res){
     res.sendFile(htmlFilePath);
 });
 
+
+
 app.get('/recommend', function(req,res){
     const htmlFilePath = path.join(__dirname,'views', 'recommend.html');
     res.sendFile(htmlFilePath);
+});
+
+
+app.post('/recommend', function(req, res) {
+    const restaurant = req.body;
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    try {
+        const fileData = fs.readFileSync(filePath);
+        const storedRestaurants = JSON.parse(fileData);
+
+        storedRestaurants.push(restaurant);
+
+        fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+        res.redirect('/confirm');
+    } catch (error) {
+        console.error('Error processing file:', error);
+        res.status(500).send('Internal Server Error');
+    }
+
 });
 
 app.get('/confirm', function(req,res){
