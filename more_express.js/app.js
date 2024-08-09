efb05f09925032fbbf91269855392225503c6019
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-09 23:40:37 +0900

    ejs를 이용하여, 사용자가 값을 입력하면, 동적으로 html의 형식안에 json으로 받은 객체값을 넣어서 렌더링함

--- more_express.js/app.js
+++ more_express.js/app.js
@@ -4,34 +4,34 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.set('views', path.join(__dirname,'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
 
 app.use(express.static('public')); //모든 수신 요청에 대해 이 공용 폴더에서 찾을 수 있는 파일에 대한 요청인지 확인해야 한다고 express에게 알림.
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.get('/restaurants', function(req,res){
-    
+app.get('/restaurants', function (req, res) {
+
 
     const filePath = path.join(__dirname, 'data', 'restaurants.json');
     const fileData = fs.readFileSync(filePath);
     const storedRestaurants = JSON.parse(fileData);
-    res.render('restaurants', {numberOfInputN: storedRestaurants.length });
+    res.render('restaurants', { numberOfInputN: storedRestaurants.length, restaurants: storedRestaurants });
 });
 
 
 
-app.get('/recommend', function(req,res){
+app.get('/recommend', function (req, res) {
     res.render('recommend');
 });
 
 
-app.post('/recommend', function(req, res) {
+app.post('/recommend', function (req, res) {
     const restaurant = req.body;
     const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
@@ -50,11 +50,11 @@ app.post('/recommend', function(req, res) {
 
 });
 
-app.get('/confirm', function(req,res){
+app.get('/confirm', function (req, res) {
     res.render('confirm');
 });
 
-app.get('/about', function(req,res){
+app.get('/about', function (req, res) {
     res.render('about');
 });
 
