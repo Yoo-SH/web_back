7c8597c3cfa5617d73e9b8c6542d5ef671d3a9d5
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-11 20:41:35 +0900

    js에서 가변범위와 쉐도잉 기법(동일한 이름의 변수)를 알 수 있는 코드 추가

--- more_js/errors.js
+++ more_js/errors.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 
 function readFile()
 {
+    let fileData
     try {
-        const fileData = fs.readFileSync('data.json');
+        const fileData = fs.readFileSync('data.json'); //같은 이름의 변수이지만, 덮어쓰기 됨. 더 높은 우선순위를 가짐, 쉐도잉 기법이라고 함.
     } catch{
         console.log('readFile error')
     }
+    console.log(fileData) //가변범위로 let fileData 적용
     console.log('readFile함수 동작')
 }
 
