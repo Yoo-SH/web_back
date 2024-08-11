c74b4f1cb421881692a16a34397c8fc001d144d0
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-11 19:12:30 +0900

    비선택적, 선택적 매개변수 활용

--- more_js/function.js
+++ more_js/function.js
@@ -1,5 +1,8 @@
-function greetUser()
+function greetUser(geetingPrefix, user = 'Yoo-SH') //비선택적 매개변수, 선택적 매개변수 (비선택적 매개변수가 , 선택적 매개변수보다 앞에 있어야함.)
 {
-    console.log('Hi js');
-    console.log('more js code');
+    console.log(geetingPrefix +' '+ user);
 }
+
+
+greetUser('hello');
+
