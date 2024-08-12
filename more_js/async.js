6dea9d6c9dd95d3722d46b6d108bd842e8f292da
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-13 06:20:07 +0900

    비동기식 코드에서 오류처리하는 방법 추가(비동기식은 try catch 사용 불가 => 매개변수로 가지게 됨

--- more_js/async.js
+++ more_js/async.js
@@ -35,7 +35,10 @@ function readFile() {
         console.log('File parsing done!')
         console.log(fileData.toString()) //콜백함수(readFile이 완료되면 함수실행) 안에 넣어서 동기식으로 처리 가능. 여러개 프로그램 돌릴 떄 이런식으로 비동기 에러 처리
     })
-    .then()
+    .then(function(){})
+    .catch(function(error) {
+        console.log(error)
+    })
 
 
     console.log('readFile error')
