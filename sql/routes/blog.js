f3856904e479ed20ad79281c6a80087763c7829c
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-16 23:58:44 +0900

    add: 데이터베이스 연결 및 데이터 쿼리를 작성

--- sql/routes/blog.js
+++ sql/routes/blog.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
+
+const db = require('../data/database'); // Import the database pool
 const router = express.Router();   // Create a new router
+
 
 router.get('/', function(req, res) {
     res.redirect('/posts');
@@ -10,8 +13,9 @@ router.get('/posts', function(req, res) {
     res.render('posts-list');
 });
 
-router.get('/new-post', function(req, res) {
-    res.render('create-post');
+router.get('/new-post', async function(req, res) {
+    const [authors] = await db.query('SELECT * FROM authors'); // db가져오는 경우, 다른 머신일 떄 시간이 걸릴 수 있음. 따라서 promise로 비동기적 처리
+    res.render('create-post', { authors : authors });
 });
 
 module.exports = router; // Export the router
