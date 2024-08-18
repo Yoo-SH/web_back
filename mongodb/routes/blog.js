e8d55f91e4a75568e51be0da63efeccf240831fb
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-18 22:27:40 +0900

    add: 블로그를 수정하면, mongodb에서 데이터를 수정하여 게시물을 변경하는 코드 추가

--- mongodb/routes/blog.js
+++ mongodb/routes/blog.js
@@ -81,4 +81,42 @@ router.post('/posts', async function (req, res) {
 
  });
 
+ router.get('/posts/:id/edit', async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+  .getDb()
+  .collection('posts')
+  .findOne({_id: new ObjectId(postId)}, {title :1 ,summary: 1, body : 1});
+
+  if(!post) {
+    return res.status(404).render('404');
+  }
+
+  res.render('update-post', { post: post });
+
+});
+
+router.post('/posts/:id/edit', async function (req, res) {
+  const postId = req.params.id;
+  const result = await db
+  .getDb()
+  .collection('posts')
+  .updateOne(
+    {_id: new ObjectId(postId)}, 
+    {$set: 
+      {
+        title: req.body.title, 
+        summary: req.body.summary, 
+        body: req.body.content
+      }
+    }
+  );
+
+  
+
+  res.redirect('/posts');
+
+});
+
+
 module.exports = router;
