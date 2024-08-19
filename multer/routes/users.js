7d6231d1ef337556458b7a99b707db3d94505860
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-19 22:07:58 +0900

    feat: multer을 이용하여 이미지를 백엔드에 가져오는 코드를 구성함

--- multer/routes/users.js
+++ multer/routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const muter = require('multer');
 
+const upload = muter({dest : 'images'});
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -9,5 +11,16 @@ router.get('/', function(req, res) {
 router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
+
+router.post('/profiles', upload.single('image'), function(req, res) {
+ const uploadedImageFile = req.file;
+ const userData = req.body;
+
+ console.log(uploadedImageFile);
+ console.log(userData);
+
+ res.redirect('/');
+});
+
 
 module.exports = router;
