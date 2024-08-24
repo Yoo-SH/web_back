cab4d6b4d1dec8a2730c1b7413dac15097419f67
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-24 13:32:40 +0900

    build: 사용자 로그인 기능 추가

--- user_login/routes/demo.js
+++ user_login/routes/demo.js
@@ -38,7 +38,34 @@ router.post('/signup', async function (req, res) {
   
 });
 
-router.post('/login', async function (req, res) {});
+router.post('/login', async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+
+  const existingUser = await db
+  .getDb()
+  .collection('users')
+  .findOne({ email : enteredEmail});
+
+  if (!existingUser) { //처음에는 이메일의 유효성을 검사
+    console.log('Invalid email');
+    return res.redirect('/login');
+  }
+
+  const passwordsAreEqual = await bcrypt.compare(
+    enteredPassword, 
+    existingUser.password
+  ); //해쉬된 비밀번호와 입력된 비밀번호를 비교
+
+  if (!passwordsAreEqual) { //이후 이메일의 우효성을 검사
+    console.log('Invalid password');
+    return res.redirect('/login');
+  }
+  
+  console.log('User logged in');
+  res.redirect('/admin');
+});
 
 router.get('/admin', function (req, res) {
   res.render('admin');
