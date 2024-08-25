2132a5e600372c2f593920899a714c4693d0fd13
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-25 18:57:59 +0900

    bulid: 미들웨어를 이용해 전역변수를 만들고, 권한에 따라 navbar를 다르게 보여주도록 코드를 구축

--- user_login/app.js
+++ user_login/app.js
@@ -30,6 +30,24 @@ app.use(session({
 }));
 
 
+app.use(async function(req, res, next) { //모든 라우트에 사용자 정보를 전달하기 위해 미들웨어를 사용한다
+  const user = req.session.user; //세션에 저장된 유저정보를 가져온다
+  const isAuth = req.session.isAuthenticated;  //세션에 저장된 어드민 정보를 가져온다
+  
+
+  if(!user || !isAuth) { //유저정보나 어드민 정보가 없다면
+    return next(); //다음 미들웨어로 넘어간다
+  }
+
+  const userDoc = await db.getDb().collection('users').findOne({_id: user.id}); //유저정보를 데이터베이스에서 찾는다
+  const isAdmin = userDoc.isAdmin; //유저정보에서 어드민 정보를 가져온다
+
+  res.locals.isAuth = isAuth; //res.locals에 어드민 정보를 저장한다
+  res.locals.isAdmin = isAdmin; //res.locals에 어드민 정보를 저장한다
+
+  next();
+});
+
 app.use(demoRoutes);
 
 app.use(function(error, req, res, next) {
