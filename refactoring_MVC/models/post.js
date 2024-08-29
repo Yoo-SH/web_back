af86e00c4cec3c05bdb6273fbe602a1f579c12b5
Yoo-SH <ads137485@jbnu.ac.kr>
2024-08-29 15:42:28 +0900

    refactor: model에 업데이트 및 삭제하는 기능 추가하여 코드를 리펙토링함

--- refactoring_MVC/models/post.js
+++ refactoring_MVC/models/post.js
@@ -1,20 +1,45 @@
+const mongodb = require('mongodb');
 const db = require('../data/database');
+
+const ObjectId = mongodb.ObjectId;
 
 class Post {
     constructor(title, content, id){
         this.title = title;
         this.content = content;
         this.id = id;
+
+        if(id){
+            this.id = new ObjectId(id)
+        }
     }
 
     async save(){
-        const result = db.getDb().collection('posts').insertOne(
-            {
+        let result;
+        if(this.id){
+            result = db.getDb().collection('posts').updateOne(
+                { _id: this.id },
+                { $set: {title : this.title , content : this.content} }
+            );
+        } else {
+            result = db.getDb().collection('posts').insertOne({
                 title: this.title,
-                content: this.content,
-            }
-        );
+                content: this.content
+            });
+        }
+        
+        
+        return result;
+    }
 
+    async delete(){
+        if(!this.id){
+            return 
+        }
+        const result = db
+        .getDb()
+        .collection('posts')
+        .deleteOne({ _id: this.id });
         return result;
     }
 }
