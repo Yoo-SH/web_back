57a2f5db13026d2e6e47d4bea885d02838f850ce
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-20 03:10:52 +0900

    add: todos 엔드포인트 post api 추가

--- ts_node/src/app.ts
+++ ts_node/src/app.ts
@@ -1,5 +1,17 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express'; // Import express on TS file, because es6 module has a import and export keyword, cf) node require and module.exports
+import { json } from 'body-parser';
+import todoRoutes from './routes/todo'; // Import todoRoutes from routes/todo.ts
+
+
 
 const app = express();
 
-app.listen(3000);
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({message: err.message});
+})
+
+app.listen(3000); 
