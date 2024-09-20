a1c6fef21df46e27e4b906906846264a481b950d
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-21 06:20:10 +0900

    add: controller를 이용하여 엔드포인트 controll

--- nest_js/src/app.controller.ts
+++ nest_js/src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller()
+@Controller('/app')
 export class AppController {
-    @Get()
+    @Get('/hello')
     getRootRoute(){
         return 'Hello nestjs!'
+    }
+
+    @Get('/bye')
+    getByeRoute(){
+        return 'Goodbye nestjs!'
     }
 }
