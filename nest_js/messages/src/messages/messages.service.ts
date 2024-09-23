028c98c4972c419af925e1c648b78190473a21b3
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-23 15:46:56 +0900

    build: 빌드 파일 수정

--- nest_js/messages/src/messages/messages.service.ts
+++ nest_js/messages/src/messages/messages.service.ts
@@ -1,8 +1,12 @@
+import { Injectable } from "@nestjs/common";
 import { MessagesRepository } from "./messages.repository";
 
+@Injectable()
 export class MessagesService {
 
-    constructor(private messagesRepo: MessagesRepository) {}
+    constructor(public messagesRepo: MessagesRepository) {
+        
+    }
     
     findOne(id: string) {
         return this.messagesRepo.findOne(id);
