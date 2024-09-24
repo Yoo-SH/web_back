8471a4152bb11565ec2ebc317362ac8c2d2c8149
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-25 01:14:53 +0900

    refactor: 중복적으로 사용되는 Serialize 클래스를 인터페이스에서 export

--- nest_js/project_car/mycv/src/users/users.controller.ts
+++ nest_js/project_car/mycv/src/users/users.controller.ts
@@ -12,7 +12,7 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
@@ -25,7 +25,7 @@ export class UsersController {
     return this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
