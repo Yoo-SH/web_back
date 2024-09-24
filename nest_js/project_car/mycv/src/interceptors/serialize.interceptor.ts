42f84d4e37aaacfb857e25a52d69b460858e4e94
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-25 01:03:43 +0900

    refactor: 인터셉트가 여러 dto를 처리할 수 있도록 생성자를 받는 식으로 수정

--- nest_js/project_car/mycv/src/interceptors/serialize.interceptor.ts
+++ nest_js/project_car/mycv/src/interceptors/serialize.interceptor.ts
@@ -7,13 +7,14 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
-import { UserDto } from '../users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     return handler.handle().pipe(
       map((data: any) => {
-        return plainToClass(UserDto, data, {
+        return plainToClass(this.dto, data, {
           excludeExtraneousValues: true, //expose 데코레이터가 붙은 필드만 반환
         });
       }),
