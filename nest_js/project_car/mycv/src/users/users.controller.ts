9f3ae4d0ba0c62f399395d5f1320d32ebaeb75ae
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-24 23:44:16 +0900

    feat: 응답속성 제외코드, exclude at 비밀번호

--- nest_js/project_car/mycv/src/users/users.controller.ts
+++ nest_js/project_car/mycv/src/users/users.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Query,
   Patch,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -22,6 +24,7 @@ export class UsersController {
     return this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
