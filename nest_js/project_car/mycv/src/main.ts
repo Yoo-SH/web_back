2a7b69a5c7b1d4aa9485810a547cafe834b9e6a6
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-24 13:02:39 +0900

    init: entity에 로그 기록을 남김

--- nest_js/project_car/mycv/src/main.ts
+++ nest_js/project_car/mycv/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); //whitelist를 설정하여, DTO에 정의되지 않은 속성을 거부
   await app.listen(3000);
 }
 bootstrap();
