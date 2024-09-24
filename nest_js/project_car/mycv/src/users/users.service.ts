41d53cc338ed750ba2e9964a82b67808caf309b0
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-24 22:43:44 +0900

    fix: http만 erro객체를 이해할 수 있기 때문에, nest NotFoundException 객체로 대체

--- nest_js/project_car/mycv/src/users/users.service.ts
+++ nest_js/project_car/mycv/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -14,17 +14,25 @@ export class UsersService {
   }
 
   findOne(id: number) {
-    return this.repo.findOne({ where: { id } });
+    const user = this.repo.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
   find(email: string) {
-    return this.repo.find({ where: { email } });
+    const user = this.repo.find({ where: { email } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
     Object.assign(user, attrs);
     return this.repo.save(user);
@@ -33,7 +41,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
     return this.repo.remove(user);
   }
