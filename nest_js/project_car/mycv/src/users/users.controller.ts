76c5cb98def2b0fd7b6226b1ffcfe6100ef5df9f
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-25 01:28:48 +0900

    chore: 인터셉터 적용 범위를 클래스로 확장

--- nest_js/project_car/mycv/src/users/users.controller.ts
+++ nest_js/project_car/mycv/src/users/users.controller.ts
@@ -15,6 +15,7 @@ import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
@@ -25,7 +26,6 @@ export class UsersController {
     return this.usersService.create(body.email, body.password);
   }
 
-  @Serialize(UserDto)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
