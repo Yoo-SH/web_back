2d0ae6bd78209eb05d853d09906d86a256fe008b
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-25 00:55:41 +0900

    init: 인터셉트를 이용하여, 사용자에게 필요한 정보 dto인스턴스만을 제공

--- nest_js/project_car/mycv/src/interceptors/serialize.interceptor.ts
+++ nest_js/project_car/mycv/src/interceptors/serialize.interceptor.ts
@@ -7,16 +7,15 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
+import { UserDto } from '../users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-    // Run something before a request is handled by the request handler
-    console.log('I am running before the handler', context);
-
     return handler.handle().pipe(
       map((data: any) => {
-        // Run something before the response is sent out
-        console.log('I am running before the response is sent out', data);
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true, //expose 데코레이터가 붙은 필드만 반환
+        });
       }),
     );
   }
