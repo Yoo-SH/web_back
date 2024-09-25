054eb21c4d5eb74fcd2d3e717ab84873aa6a57b0
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-25 17:31:54 +0900

    init: 회원가입 service 구축

---
+++ nest_js/project_car/mycv/src/users/auth.service.ts
@@ -0,0 +1,34 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {} //usersService 의존성 주입
+
+  async signUp(email: string, password: string) {
+    //이메일 중복 확인
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email in use');
+    }
+
+    //해쉬된 패스워드 생성
+
+    //솔트 생성
+    const salt = randomBytes(8).toString('hex');
+    //비밀번호와 솔트를 해쉬화하여 저장
+    const hash = (await scrypt(password, salt, 32)) as Buffer; //타입스크립트는 scrypt가 뭐하는 지 인식할 수 있으니 buffer로 지정
+    //해쉬된 비밀번호와 솔트를 조인
+    const result = salt + '.' + hash.toString('hex'); //hash는 buffer이므로 16진수 string으로 변환
+
+    // 유저 생성 및 저장
+    const user = await this.usersService.create(email, result);
+
+    //
+    return user;
+  }
+}
--- nest_js/project_car/mycv/src/users/users.module.ts
+++ nest_js/project_car/mycv/src/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 import { User } from './user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])], //모듈에 TypeOrmModule.forFeature()를 사용하여 엔티티를 가져옴, 모듈에 레파지토리 생성
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule {}
