ecac7ed46abdb0399b3229f09bd7877eb1779602
Yoo-SH <ads137485@jbnu.ac.kr>
2024-09-26 14:06:30 +0900

    init: 데코레이터와 전역적 인터셉터를 이용해, 핸들러에게 누가 로그인 되어있는지 자동적으로 알려주도록 설정

--- nest_js/project_car/mycv/src/users/users.controller.ts
+++ nest_js/project_car/mycv/src/users/users.controller.ts
@@ -15,6 +15,8 @@ import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from '../decorator/current-user.decoraotr';
+import { User } from './user.entity';
 
 @Serialize(UserDto)
 @Controller('auth')
@@ -35,13 +37,32 @@ export class UsersController {
   async signIn(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signIn(body.email, body.password);
     session.userId = user.id;
+    console.log('Session after sign in:', session); // 세션 로그 확인
     return user;
   }
 
   @Post('/signout')
   signOut(@Session() session: any) {
+    console.log('Signout route called'); // 로그아웃 라우트 호출 확인
+
     session.userId = null;
+    console.log('Session after signout:', session); // 로그아웃 후 세션 확인
   }
+
+  @Get('/whoami')
+  whoAmI(@CurrentUser() user: User) {
+    console.log('whoAmI route called'); // 메서드가 호출되는지 확인하기 위한 로그 추가
+
+    return user;
+  }
+  /* 
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    console.log('whoAmI route called'); // 메서드가 호출되는지 확인하기 위한 로그 추가
+    console.log('Session UserId:', session.userId); // session.userId 확인
+
+    return this.usersService.findOne(session.userId);
+  } */
 
   @Get('/:id')
   findUser(@Param('id') id: string) {
@@ -51,11 +72,6 @@ export class UsersController {
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
-  }
-
-  @Get('/whoami') //세션을 사용하여 현재 사용자의 정보를 가져옴
-  whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
   }
 
   @Delete('/:id')
