eae9c7997cf2e55fe936c7170051b207d286149b
Yoo-SH <ads137485@jbnu.ac.kr>
2025-02-10 16:27:30 +0900

    feat:@Value annotation을 이용한 사용자 정의 변수 호출

--- springboot/src/main/java/com/springboot/springboot/rest/FunRestController.java
+++ springboot/src/main/java/com/springboot/springboot/rest/FunRestController.java
@@ -1,21 +1,27 @@
 package com.springboot.springboot.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+
     @GetMapping("/")
     public String SayHello() {
         return "Hello World test";
     }
 
-    @GetMapping("/test")
-    public String test() {
-        System.out.println("testZ");
-        return "Hell";
+    @GetMapping("/team")
+    public String getTeamInfo() {
+        return "Team Name: " + teamName + " Coach Name: " + coachName;
     }
-
 
 }
