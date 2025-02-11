0873d46f3e4beaccfa967b6811843c8fa7eda453
Yoo-SH <ads137485@jbnu.ac.kr>
2025-02-12 00:56:04 +0900

    feat: beanScope 테스트

--- spring/springCore/src/main/java/com/springCore/springCore/common/TennisCoach.java
+++ spring/springCore/src/main/java/com/springCore/springCore/common/TennisCoach.java
@@ -1,11 +1,15 @@
 package com.springCore.springCore.common;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 
 @Component
 @Primary
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TennisCoach implements Coach {
     public TennisCoach() {
         System.out.println("TennisCoach constructor");
--- spring/springCore/src/main/java/com/springCore/springCore/rest/DemoController.java
+++ spring/springCore/src/main/java/com/springCore/springCore/rest/DemoController.java
@@ -10,14 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
+    private Coach mySubCoach;
 
     @Autowired
-    public void setMyCoach(Coach theCoach) {
+    public void setMyCoach(@Qualifier("tennisCoach") Coach theCoach, @Qualifier("tennisCoach") Coach theSubCoach ) {
         myCoach = theCoach;
+        mySubCoach = theSubCoach;
     }
 
     @GetMapping("/practice")
     public String getDailyWorkOut() {
+        System.out.println("protoType test: " + (myCoach==mySubCoach));
         return myCoach.getDailyWorkOut();
     }
 
