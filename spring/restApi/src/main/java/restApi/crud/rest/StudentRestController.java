9f67866bb3e48b79ef55a1237c135d4ac205e5eb
Yoo-SH <ads137485@jbnu.ac.kr>
2025-02-13 15:21:07 +0900

    feat: path variable api data_binding 테스트

--- spring/restApi/src/main/java/restApi/crud/rest/StudentRestController.java
+++ spring/restApi/src/main/java/restApi/crud/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package restApi.crud.rest;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import restApi.crud.entity.Student;
@@ -31,5 +32,11 @@ public class StudentRestController {
 
         return theStudents;
 
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        return theStudents.get(studentId);
     }
 }
