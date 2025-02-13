ed8b5547e5983024b9be7f95aa1731d911a82f7e
Yoo-SH <ads137485@jbnu.ac.kr>
2025-02-13 15:17:23 +0900

    refactor: data binding test 리펙토링

--- spring/restApi/src/main/java/restApi/crud/rest/StudentRestController.java
+++ spring/restApi/src/main/java/restApi/crud/rest/StudentRestController.java
@@ -1,5 +1,7 @@
 package restApi.crud.rest;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +14,20 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<Student>();
+    private List<Student> theStudents;
+
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("John", "Doe"));
         theStudents.add(new Student("Jana", "Doe"));
         theStudents.add(new Student("lani", "Doe"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return theStudents;
 
