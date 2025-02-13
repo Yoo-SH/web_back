70d6a710203e7f5bf3da610efd21e8ccee61fdee
Yoo-SH <ads137485@jbnu.ac.kr>
2025-02-13 09:37:17 +0900

    feat:JPA CRUD test

--- spring/jpa/src/main/java/spring_jpa/demo/dao/StudentDAO.java
+++ spring/jpa/src/main/java/spring_jpa/demo/dao/StudentDAO.java
@@ -2,7 +2,21 @@ package spring_jpa.demo.dao;
 
 import spring_jpa.demo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
-    void save(Student student);
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    int deleteAll();
 }
