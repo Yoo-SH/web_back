5280e3731cb432b422a21ff8e8bfefdf5f47571f
Yoo-SH <ads137485@jbnu.ac.kr>
2025-02-17 20:36:52 +0900

    build: mvc_validation 빌드 설정

---
+++ spring/mvb_validataion/src/test/java/spring/mvb_validataion/MvbValidataionApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.mvb_validataion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvbValidataionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
